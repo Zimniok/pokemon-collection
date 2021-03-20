a4b799dfc9d53121c97c7b51bc0bcbb5a0d5f421
Sejwo <sawickimicha8@outlook.com>
2021-03-20 03:14:33 +0100

    Wyswietlanie listy(messy)

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,8 @@
+function showList(res){
+  for (var i = 0;i < res.data.length;i++){
+    document.getElementById('div1').appendChild(document.createElement("div").appendChild(document.createTextNode('id: ' + res.data[i].id + ' name: '+ res.data[i].name+'\r\n')))
+  } 
+}
 var para = document.createElement("p");
 var node = document.createTextNode("This is new.");
 para.appendChild(node);
@@ -10,4 +15,5 @@ element.appendChild(para);
 const url='http://localhost:5000/all_cards';
 fetch(url)
 .then(data=>{return data.json()})
-.then(res=>{console.log(res)})
+.then(res=>{showList(res)})
+
