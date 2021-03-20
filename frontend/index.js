3b9b551ba000eb95803532711cc3276dc1f2f015
Sejwo <sawickimicha8@outlook.com>
2021-03-20 02:54:59 +0100

    I did a thing

--- frontend/index.js
+++ frontend/index.js
@@ -4,3 +4,10 @@ para.appendChild(node);
 
 var element = document.getElementById("div1");
 element.appendChild(para);
+
+
+
+const url='http://localhost:5000/all_cards';
+fetch(url)
+.then(data=>{return data.json()})
+.then(res=>{console.log(res)})
